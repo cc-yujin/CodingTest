fe762ef137cdfbe2251b2a73d23889d6f7deb2cf
HANYUJIN <mooo7570@gmail.com>
2023-05-25 11:32:06 +0900

    230525

---
+++ JS-100days-challenge/120891.369게임.js
@@ -0,0 +1,23 @@
+function solution(order) {
+  let num = order.toString().split(''); // 숫자가 하나씩 분리 되어 배열에 저장
+  let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] == '3' || num[i] == '6' || num[i] == '9') {
+      sum++;
+    }
+  }
+  return sum;
+}
+
+//match
+function solution(order) {
+  return order.toString().match(/[369]/g).length;
+}
+
+//테스트케이스 통과
+function solution(order) {
+  let value = order.toString().match(/[369]/g) ?? [];
+  return value.length;
+}
+
+// set
